a39002c50ff5d9529e9e58a66213c88955b1e48b
yeasin50 <46500228+yeasin50@users.noreply.github.com>
2025-05-23 01:54:35 +0600

    removed-dotfiles

--- nvim/lua/config/plugins/surround.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "kylechui/nvim-surround",
-    version = "*", -- use the latest
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({})
-    end,
-}
--- nvim/lua/config/plugins/vim_be_good.lua
+++
@@ -1,9 +0,0 @@
--- lua/plugins/vim_be_good.lua
-return {
-  {
-    "ThePrimeagen/vim-be-good",
-    cmd = "VimBeGood",  -- Optional: only loads plugin when this command is used
-    lazy = true,
-  }
-}
-
